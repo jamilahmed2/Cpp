a501048a3ff30e41e85068d826e732264e453a2f
jamilahmed2 <ja0935991@gmail.com>
2024-06-18 22:21:45 +0500

    loop through an array

--- arrays.cpp
+++ arrays.cpp
@@ -14,3 +14,19 @@ int main(){
 
     return 0;
 }
+
+
+// loop through an array
+#include<iostream>
+int main(){
+    std::string cars[3]={"Volvo","BMW","Audi"};
+    for(int i =0; i<3;i++){
+        std::cout<< i << "=" << cars[i]<<std::endl;
+    }
+    return 0;
+}
+
+// output
+// 0=Volvo
+// 1=BMW
+// 2=Audi
