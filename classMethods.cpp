d15b6bd196c9a4edc43b60db1fc951169fc621f4
jamilahmed2 <ja0935991@gmail.com>
2024-06-21 22:46:30 +0500

    parameters

--- classMethods.cpp
+++ classMethods.cpp
@@ -18,21 +18,42 @@
 
 
 // outside 
+// #include <iostream>
+// using namespace std;
+
+// class MyClass {         // The class
+//   public:               // Access specifier
+//     void myMethod();    // Method/function declaration
+// };
+
+// // Method/function definition outside the class
+// void MyClass::myMethod() {
+//   cout << "Hello World!";
+// }
+
+// int main() {
+//   MyClass myObj;     // Create an object of MyClass
+//   myObj.myMethod();  // Call the method
+//   return 0;
+// }
+
+
+
+// Parameters
 #include <iostream>
 using namespace std;
 
-class MyClass {         // The class
-  public:               // Access specifier
-    void myMethod();    // Method/function declaration
+class Car {
+  public:
+    int speed(int maxSpeed);
 };
 
-// Method/function definition outside the class
-void MyClass::myMethod() {
-  cout << "Hello World!";
+int Car::speed(int maxSpeed) {
+  return maxSpeed;
 }
 
 int main() {
-  MyClass myObj;     // Create an object of MyClass
-  myObj.myMethod();  // Call the method
+  Car myObj; // Create an object of Car
+  cout << myObj.speed(10); // Call the method with an argument
   return 0;
 }
