9e1c3be007efe55e37802be207e9d0bef050b1b4
jamilahmed2 <ja0935991@gmail.com>
2024-06-21 22:44:49 +0500

    outside

--- classMethods.cpp
+++ classMethods.cpp
@@ -1,13 +1,35 @@
 // Inside method
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+//     class MyClass {        // The class
+//   public:              // Access specifier
+//     void myMethod() {  // Method/function defined inside the class
+//       cout << "Hello World!";
+//     }
+// };
+
+// int main() {
+//   MyClass myObj;     // Create an object of MyClass
+//   myObj.myMethod();  // Call the method
+//   return 0;
+// }
+
+
+
+// outside 
+#include <iostream>
 using namespace std;
 
-    class MyClass {        // The class
-  public:              // Access specifier
-    void myMethod() {  // Method/function defined inside the class
-      cout << "Hello World!";
-    }
+class MyClass {         // The class
+  public:               // Access specifier
+    void myMethod();    // Method/function declaration
 };
+
+// Method/function definition outside the class
+void MyClass::myMethod() {
+  cout << "Hello World!";
+}
 
 int main() {
   MyClass myObj;     // Create an object of MyClass
