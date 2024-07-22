12306714c117c88cdd1506ff174de7013bbcd924
jamilahmed2 <ja0935991@gmail.com>
2024-07-23 01:39:54 +0500

    operators

---
+++ operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1;
+    int num2;
+    int result;
+
+    cout<<"Enter Number 1: ";
+    cin>>num1;
+
+    cout<<"Enter Number 2: ";
+    cin>>num2;
+
+    result = num1 - num2;
+    // result = num1 + num2;
+    // result = num1 / num2;
+    // result = num1 * num2;
+    // result = num1 % num2;
+
+    return 0;
+}
